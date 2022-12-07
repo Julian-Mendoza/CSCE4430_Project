a58b82a8767b28db8da9658857506f5ad9a30419
rumduth <96284452+rumduth@users.noreply.github.com>
2022-12-07 03:15:02 -0600

    Add files via upload

---
+++ FitnessApp_CSCE4430/Pages/SettingPage.xaml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingPage : ContentPage
+    {
+        public SettingPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Goal_Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Goal());
+        }
+    }
+}
