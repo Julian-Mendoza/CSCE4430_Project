837cc99f310c4e66e0d0a0e74436773242022d05
IsabelC2000 <77826975+IsabelC2000@users.noreply.github.com>
2022-12-06 20:56:01 -0600

    Add files via upload

---
+++ FitnessApp_CSCE4430/Pages/PersonalData.xaml.cs
@@ -0,0 +1,62 @@
+using ProjectTest.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FitnessApp_CSCE4430.Pages 
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PersonalData : ContentPage
+    {
+       Person testData = new Person("Julian", "jjm", "Password", true); 
+       public PersonalData()
+        {
+            InitializeComponent();
+        }
+
+        public class weightGraph
+        {
+            public string Date {get; set;}
+            public double Weight {get; set;}
+
+            public weightGraph(string xValue, double yValue)
+            {
+                Date = xValue;
+                Weight = yValue; 
+            }
+        }
+
+        public class viewGraph
+        {
+            public ObservableCollection<weightGraph> userData{get; set;}
+
+            public viewGraph()
+            {
+                userData = new ObservableCollection<weightGraph>()
+                {
+                    //Add random dates and weights
+                    new weightGraph("January 17", 200);
+                    new weightGraph("January 29", 195);
+                    new weightGraph("February 10", 190);
+                    new weightGraph("February 21", 185);
+                    new weightGraph("March 16", 180);
+                    new weightGraph("March 23", 175);
+                    new weightGraph("April 15", 170);
+                    new weightGraph("April 25", 165);
+                    new weightGraph("May 12", 160);
+                    new weightGraph("May 27", 155);
+                    new weigthGraph("June 3", 150);
+                    new weightGraph("June 21", 145);
+                    new weightGraph("July 6", 140);
+                    new weightGraph("July 20", 135); 
+                };
+            }
+        }
+
+    }
+}
