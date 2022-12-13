47d2b84c4782f42bd430fe68f8774d57a4767bb4
IsabelC2000 <77826975+IsabelC2000@users.noreply.github.com>
2022-12-12 19:46:26 -0600

    Update PersonalData.xaml.cs

--- FitnessApp_CSCE4430/Pages/PersonalData.xaml.cs
+++ FitnessApp_CSCE4430/Pages/PersonalData.xaml.cs
@@ -1,4 +1,4 @@
-using ProjectTest.Classes;
+using FitnessApp_CSCE4430.Classes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +12,7 @@ namespace FitnessApp_CSCE4430.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PersonalData : ContentPage
-    {
-       Person testData = new Person("Julian", "jjm", "Password", true); 
+    { 
        public PersonalData()
         {
             InitializeComponent();
@@ -40,20 +39,20 @@ namespace FitnessApp_CSCE4430.Pages
                 userData = new ObservableCollection<weightGraph>()
                 {
                     //Add random dates and weights
-                    new weightGraph("January 17", 200);
-                    new weightGraph("January 29", 195);
-                    new weightGraph("February 10", 190);
-                    new weightGraph("February 21", 185);
-                    new weightGraph("March 16", 180);
-                    new weightGraph("March 23", 175);
-                    new weightGraph("April 15", 170);
-                    new weightGraph("April 25", 165);
-                    new weightGraph("May 12", 160);
-                    new weightGraph("May 27", 155);
-                    new weigthGraph("June 3", 150);
-                    new weightGraph("June 21", 145);
-                    new weightGraph("July 6", 140);
-                    new weightGraph("July 20", 135); 
+                    new weightGraph("January 17", 200),
+                    new weightGraph("January 29", 195),
+                    new weightGraph("February 10", 190),
+                    new weightGraph("February 21", 185),
+                    new weightGraph("March 16", 180),
+                    new weightGraph("March 23", 175),
+                    new weightGraph("April 15", 170),
+                    new weightGraph("April 25", 165),
+                    new weightGraph("May 12", 160),
+                    new weightGraph("May 27", 155),
+                    new weightGraph("June 3", 150),
+                    new weightGraph("June 21", 145),
+                    new weightGraph("July 6", 140),
+                    new weightGraph("July 20", 135), 
                 };
             }
         }
